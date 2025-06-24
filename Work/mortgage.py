a97debea8fd7b4e7a687fa210529c726746c8ba8
Alexander Lu <alu0117@gmail.com>
2025-06-23 18:35:54 -0700

    Completed Exercise 1.10. Fixed bug with principal calculations.

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -13,12 +13,15 @@ extra_payment_end_month = 108
 extra_payment = 1000
 
 while month <= 342:
-    principal = principal*(1+rate/12)-payment
+    principal = principal*(1+rate/12)
     if month >= extra_payment_start_month and month <= extra_payment_end_month:
         principal = principal - (payment + extra_payment)
         total_paid = total_paid + (payment + extra_payment)
     else:
+        principal = principal - payment
         total_paid = total_paid + payment
+
+    print(month, round(total_paid,2), round(principal,2))
     month += 1
 
 print('Total paid', total_paid)
