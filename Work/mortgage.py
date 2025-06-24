7b6ffbfeda85969399b0be533a37926f79364a90
Alexander Lu <alu0117@gmail.com>
2025-06-23 18:43:53 -0700

    Completed Exercise 1.11. Corrects for overpayment

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -12,17 +12,21 @@ extra_payment_start_month = 61
 extra_payment_end_month = 108
 extra_payment = 1000
 
-while month <= 342:
+while month <= months:
     principal = principal*(1+rate/12)
     if month >= extra_payment_start_month and month <= extra_payment_end_month:
         principal = principal - (payment + extra_payment)
+        if principal < 0:
+            break
         total_paid = total_paid + (payment + extra_payment)
     else:
         principal = principal - payment
+        if principal < 0:
+            break
         total_paid = total_paid + payment
-
+   
     print(month, round(total_paid,2), round(principal,2))
     month += 1
 
-print('Total paid', total_paid)
+print('Total paid', round(total_paid,2))
 
