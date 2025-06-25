070a834bb3fd6e1a2e9c653188a9ac1bafb8681b
Alexander Lu <alu0117@gmail.com>
2025-06-24 21:18:58 -0700

    Completed Exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,21 @@
 # pcost.py
 #
 # Exercise 1.27
+path = 'Data/portfolio.csv'
+f = open(path, 'rt')
+headers = next(f) # skip headers
+cost = 0
+for line in f:
+    # convert line to list
+    row =  line.split(',')
+
+    # extract number of shares
+    shares = int(row[1])
+
+    #extract price
+    price = float(row[2])
+    
+    total = shares*price
+    cost += total
+print('Total cost', cost)
+f.close()
