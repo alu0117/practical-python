8c7f6deeb695f41e123dc0b0b1f6b71d0b71fd1b
Alexander Lu <alu0117@gmail.com>
2025-06-24 21:23:38 -0700

    Removed whitespace between lines

--- Work/pcost.py
+++ Work/pcost.py
@@ -8,13 +8,10 @@ cost = 0
 for line in f:
     # convert line to list
     row =  line.split(',')
-
     # extract number of shares
     shares = int(row[1])
-
     #extract price
     price = float(row[2])
-    
     total = shares*price
     cost += total
 print('Total cost', cost)
