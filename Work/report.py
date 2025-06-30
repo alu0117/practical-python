863596a876f7ab776f5e57fe34332557dc99945f
Alexander Lu <alu0117@gmail.com>
2025-06-29 18:33:01 -0700

    Generalized csv read for any column format. Completed Exercise 2.16

--- Work/report.py
+++ Work/report.py
@@ -7,9 +7,10 @@ def read_portfolio(filename):
 
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
-        next(rows)
+        headers = next(rows)
         for row in rows:
-            portfolio.append({'name':row[0], 'shares': int(row[1]),'price': float(row[2])})
+            record = dict(zip(headers,row))
+            portfolio.append({'name':record['name'], 'shares': int(record['shares']),'price': float(record['price'])})
     return portfolio
 
 def read_prices(filename):
