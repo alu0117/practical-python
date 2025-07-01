95f140669127480eea60474219caf2e9ee4e2c8d
Alexander Lu <alu0117@gmail.com>
2025-07-01 11:05:11 -0700

    Completed Exercuse 3.7

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -2,25 +2,28 @@
 #
 # Exercise 3.3
 import csv
-def parse_csv(filename, select = None, types = [str, int, float]):
+def parse_csv(filename, select = None, types = [str, int, float], has_headers = True, delimiter= ',') -> list: 
     '''
     Parse a CSV file into a list of record
     '''
     with open(filename, 'rt') as f:
-        rows = csv.reader(f)
-
-        headers = next(rows)
-        indices = list(range(len(headers))) #default indices if no selection 
-        if select:
-            indices = [headers.index(s) for s in select]
-        else:
-            select = headers
-
+        rows = csv.reader(f, delimiter=delimiter)
+        indices = list(range(len(types))) #default indices if no selection
+        if has_headers:
+            headers = next(rows) 
+            if select:
+                indices = [headers.index(s) for s in select]
+            else:
+                select = headers
         records = []
         for row in rows:
+            record = None
             if not row: #Skip rows with no data
                 continue
-            record = {col:func(row[ndx]) for col,ndx,func in zip(select,indices,types)}
+            if has_headers:
+                record = {col:func(row[ndx]) for col,ndx,func in zip(select,indices,types)}
+            else:
+                record = tuple([func(val) for func, val in zip(types,row)])
             records.append(record)
     return records
  
